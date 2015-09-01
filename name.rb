b8da00c00a49adbc216338d03d410a8265653340
Anton Malkov <anton.malkov@gmail.com>
2015-09-01 21:14:19 +0200

    Add fist/last name and 10.times to name.rb

--- name.rb
+++ name.rb
@@ -1,5 +1,11 @@
-puts "\nEnter your name: "
+puts "\nEnter your first name:"
+first_name = gets.chomp
 
-name = gets.chomp
+puts "\nEnter your last name:"
+last_name = gets.chomp
 
-puts "\nHello, my dear " + name + "!\n\n"
+name = first_name + " " + last_name
+
+puts "\nHello, my dear #{name}!\n\n"
+
+10.times { puts "Remember, your name is #{name}" }
